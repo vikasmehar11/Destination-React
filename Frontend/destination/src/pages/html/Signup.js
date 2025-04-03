8387d6cda8f288722873b161acb67df372d5e5e5
vikasmehar11 <123346070+vikasmehar11@users.noreply.github.com>
2025-04-03 23:57:03 +0530

    Update Signup.js

--- Frontend/destination/src/pages/html/Signup.js
+++ Frontend/destination/src/pages/html/Signup.js
@@ -79,8 +79,8 @@ const Signup = () => {
           style={{
             color: 'wheat',
             position: 'absolute',
-            marginTop: '1%',
-            marginLeft: '32%',
+            marginTop: '2%',
+            marginLeft: '56%',
             zIndex: 2,
             fontFamily: '"Brush Script MT", cursive',
             fontSize: '2rem',
