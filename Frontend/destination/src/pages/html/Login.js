2c254333549bee7d88a5b550f996ff0f2d0538e0
vikasmehar11 <123346070+vikasmehar11@users.noreply.github.com>
2025-04-03 18:18:50 +0530

    Update Login.js

--- Frontend/destination/src/pages/html/Login.js
+++ Frontend/destination/src/pages/html/Login.js
@@ -20,7 +20,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch('/destination/login', {
+      const response = await fetch('https://destination-react-backend.onrender.com/destination/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
