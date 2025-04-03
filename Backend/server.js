dab5cb3d55cbeca5ce716ac864741f55ddc2df23
vikasmehar11 <123346070+vikasmehar11@users.noreply.github.com>
2025-04-03 22:51:27 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -6,12 +6,16 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 const cors = require('cors')
 const db = require('./utils/db')
+const corsOptions = {
+  origin: 'https://destination-react-frontend.vercel.app', 
+  credentials: true, 
+};
 
 db()
 dotenv.config({path: './config/.env'})
 app.use(express.json())
 app.use(morgan('tiny'))
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use('/destination', require('./router/destinationRouter'))
 app.use('/destination', require('./router/bookingRouter'));
