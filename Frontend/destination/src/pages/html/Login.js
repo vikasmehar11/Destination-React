b71a7e1d32e282cb9b85d41155179fa0b597beba
vikasmehar11 <123346070+vikasmehar11@users.noreply.github.com>
2025-04-05 01:29:08 +0530

    Update Login.js

--- Frontend/destination/src/pages/html/Login.js
+++ Frontend/destination/src/pages/html/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
+import axios from "axios";
 
 const Login = () => {
   const [email, setEmail] = useState('');
